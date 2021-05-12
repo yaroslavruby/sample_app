98e9ed81a1fd627da25c6cd1c1186336833d9fb8
yaroslavruby <ohrimchuk77@gmail.com>
2021-05-12 16:31:24 +0200

    Fix host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,30 +1,18 @@
 Rails.application.configure do
-  
+
   config.cache_classes = true
-
   config.eager_load = true
-
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
- 
   config.assets.compile = false
-
   config.active_storage.service = :local
-
   config.force_ssl = true
-  
   config.log_level = :debug
-  
   config.log_tags = [ :request_id ]
- 
   config.action_mailer.perform_caching = false
-  
   config.i18n.fallbacks = true
-  
   config.active_support.deprecation = :notify
- 
   config.log_formatter = ::Logger::Formatter.new
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
@@ -35,18 +23,19 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
 
-#   config.action_mailer.raise_delivery_errors = true
-#   config.action_mailer.delivery_method = :smtp
-#   host = 'https://gentle-reaches-71975.herokuapp.com'
-#   config.action_mailer.default_url_options = { host: host }
-#   ActionMailer::Base.smtp_settings = {
-#     :address              => 'smtp.sendgrid.net',
-#     :port                 => '587',
-#     :authentication       => :plain,
-#     :user_name            => ENV['SENDGRID_USERNAME'],
-#     :password             => ENV['SENDGRID_PASSWORD'],
-#     :domain               => 'heroku.com',
-#     :enable_starttls_auto => true
-#   }
-  
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://secure-earth-28706.herokuapp.com/'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address              => 'smtp.sendgrid.net',
+    :port                 => '587',
+    :authentication       => :plain,
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
+    :domain               => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
